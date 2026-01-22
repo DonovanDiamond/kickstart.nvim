80d4925b539a627402d4a7d29f5776e45824dcc0
Donovan Diamond <dds@cloaf.africa>
2026-01-22 11:41:03 +0200

    Disable gopls and Go tools in init.lua
    Comment out gopls and related Go tools in the configuration.

--- init.lua
+++ init.lua
@@ -612,7 +612,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
+        -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -656,11 +656,11 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'goimports',
-        'golangci-lint',
-        'golangci-lint-langserver',
-        'gopls',
-        'gotests',
+        --'goimports',
+        --'golangci-lint',
+        --'golangci-lint-langserver',
+        --'gopls',
+        --'gotests',
         --'prettier',
         --'svelte-language-server',
         --'typescript-language-server',
